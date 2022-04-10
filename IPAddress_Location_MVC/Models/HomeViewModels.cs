2fd43b3216091de103a2145c5e57d5b2f14fb94b
ritiktailor09 <ritiktailor09@gmail.com>
2022-04-10 14:11:42 +0400

    Added packages

---
+++ IPAddress_Location_MVC/Models/HomeViewModels.cs
@@ -0,0 +1,568 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IPAddress_Location_MVC.Models
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Btc
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Eth
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Ltc
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Bch
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Bnb
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Eos
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Xrp
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Xlm
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Link
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Dot
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Yfi
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Usd
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Aed
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Ars
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Aud
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Bdt
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Bhd
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Bmd
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Brl
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Cad
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Chf
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Clp
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Cny
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Czk
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Dkk
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Eur
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Gbp
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Hkd
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Huf
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Idr
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Ils
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Inr
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Jpy
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Krw
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Kwd
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Lkr
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Mmk
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Mxn
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Myr
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Ngn
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Nok
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Nzd
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Php
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Pkr
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Pln
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Rub
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Sar
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Sek
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Sgd
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Thb
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Try
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Twd
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Uah
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Vef
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Vnd
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Zar
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Xdr
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Xag
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Xau
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Bits
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Sats
+    {
+        public string name { get; set; }
+        public string unit { get; set; }
+        public double value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Rates
+    {
+        public Btc btc { get; set; }
+        public Eth eth { get; set; }
+        public Ltc ltc { get; set; }
+        public Bch bch { get; set; }
+        public Bnb bnb { get; set; }
+        public Eos eos { get; set; }
+        public Xrp xrp { get; set; }
+        public Xlm xlm { get; set; }
+        public Link link { get; set; }
+        public Dot dot { get; set; }
+        public Yfi yfi { get; set; }
+        public Usd usd { get; set; }
+        public Aed aed { get; set; }
+        public Ars ars { get; set; }
+        public Aud aud { get; set; }
+        public Bdt bdt { get; set; }
+        public Bhd bhd { get; set; }
+        public Bmd bmd { get; set; }
+        public Brl brl { get; set; }
+        public Cad cad { get; set; }
+        public Chf chf { get; set; }
+        public Clp clp { get; set; }
+        public Cny cny { get; set; }
+        public Czk czk { get; set; }
+        public Dkk dkk { get; set; }
+        public Eur eur { get; set; }
+        public Gbp gbp { get; set; }
+        public Hkd hkd { get; set; }
+        public Huf huf { get; set; }
+        public Idr idr { get; set; }
+        public Ils ils { get; set; }
+        public Inr inr { get; set; }
+        public Jpy jpy { get; set; }
+        public Krw krw { get; set; }
+        public Kwd kwd { get; set; }
+        public Lkr lkr { get; set; }
+        public Mmk mmk { get; set; }
+        public Mxn mxn { get; set; }
+        public Myr myr { get; set; }
+        public Ngn ngn { get; set; }
+        public Nok nok { get; set; }
+        public Nzd nzd { get; set; }
+        public Php php { get; set; }
+        public Pkr pkr { get; set; }
+        public Pln pln { get; set; }
+        public Rub rub { get; set; }
+        public Sar sar { get; set; }
+        public Sek sek { get; set; }
+        public Sgd sgd { get; set; }
+        public Thb thb { get; set; }
+        public Try _try { get; set; }
+        public Twd twd { get; set; }
+        public Uah uah { get; set; }
+        public Vef vef { get; set; }
+        public Vnd vnd { get; set; }
+        public Zar zar { get; set; }
+        public Xdr xdr { get; set; }
+        public Xag xag { get; set; }
+        public Xau xau { get; set; }
+        public Bits bits { get; set; }
+        public Sats sats { get; set; }
+    }
+
+    public class Root
+    {
+        public Rates rates { get; set; }
+    }
+
+
+}
